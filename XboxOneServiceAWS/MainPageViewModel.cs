fb297a7aefb1f682b867a4795d4c08d8fd2a7e25
Daniel Valcarce <davamix@gmail.com>
2016-12-11 14:27:14 +0100

    Added search box and command

--- XboxOneServiceAWS/MainPageViewModel.cs
+++ XboxOneServiceAWS/MainPageViewModel.cs
@@ -1,107 +1,149 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Windows.Data.Xml.Dom;
+using XboxOneServiceAWS.Models;
 
 namespace XboxOneServiceAWS
 {
+    public class DelegateCommand : ICommand
+    {
+        private readonly Predicate<object> _canExecute;
+        private readonly Action<string> _execute;
+
+        public DelegateCommand(Predicate<object> canExecute, Action<string> execute)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((string)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+
     public class MainPageViewModel
     {
-		private static readonly string AWS_ACCESS_KEY_ID = "***";
-		private static readonly string AWS_SECRET_KEY = "***";
-	    private static readonly string ASSOCIATE_TAG = "***";
-		private static readonly string ENDPOINT = "webservices.amazon.es";
+        private static readonly string AWS_ACCESS_KEY_ID = "***";
+        private static readonly string AWS_SECRET_KEY = "***";
+        private static readonly string ASSOCIATE_TAG = "***";
+        private static readonly string ENDPOINT = "webservices.amazon.es";
 
-		public ObservableCollection<string> Values { get; }
+        public ObservableCollection<TitleItem> Values { get; }
+
+        public ICommand Search { get; set; }
 
         public MainPageViewModel()
         {
-            Values = new ObservableCollection<string>();
+            Values = new ObservableCollection<TitleItem>();
 
-	        GetQueryUrl()
-		        .ContinueWith(RunQuery)
-		        .ContinueWith(x => ParseResults(x.Result))
-		        .ContinueWith(x => ShowValues(x.Result),
-			        CancellationToken.None,
-			        TaskContinuationOptions.OnlyOnRanToCompletion,
-			        TaskScheduler.FromCurrentSynchronizationContext());
+            Search = new DelegateCommand(x => true, SearchInAmazon);
+        }
+
+        private void SearchInAmazon(string keywords)
+        {
+            GetQueryUrl(keywords)
+                .ContinueWith(RunQuery)
+                .ContinueWith(x => ParseResults(x.Result))
+                .ContinueWith(x => ShowValues(x.Result),
+                    CancellationToken.None,
+                    TaskContinuationOptions.OnlyOnRanToCompletion,
+                    TaskScheduler.FromCurrentSynchronizationContext());
 
         }
 
-		private async Task<string> GetQueryUrl()
-		{
-			SignedRequestHelper signedHelper;
+        private async Task<string> GetQueryUrl(string keywords)
+        {
+            SignedRequestHelper signedHelper;
 
-			try
-			{
-				signedHelper = new SignedRequestHelper(AWS_ACCESS_KEY_ID, AWS_SECRET_KEY, ENDPOINT);
-			}
-			catch (Exception ex)
-			{
-				Debug.WriteLine(ex.Message);
-				return string.Empty;
-			}
+            try
+            {
+                signedHelper = new SignedRequestHelper(AWS_ACCESS_KEY_ID, AWS_SECRET_KEY, ENDPOINT);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return string.Empty;
+            }
 
-			var parameters = new Dictionary<string, string>
-			{
-				{"Service", "AWSECommerceService"},
-				{"Operation", "ItemSearch"},
-				{"AWSAccessKeyId", AWS_ACCESS_KEY_ID},
-				{"AssociateTag", ASSOCIATE_TAG},
-				{"SearchIndex", "DVD"},
-				{"ResponseGroup", "Images,ItemAttributes"},
-				{"Sort", "price"},
-				{"Keywords", "Toy Story"}
-			};
+            var parameters = new Dictionary<string, string>
+            {
+                {"Service", "AWSECommerceService"},
+                {"Operation", "ItemSearch"},
+                {"AWSAccessKeyId", AWS_ACCESS_KEY_ID},
+                {"AssociateTag", ASSOCIATE_TAG},
+                {"SearchIndex", "DVD"},
+                {"ResponseGroup", "Images,ItemAttributes"},
+                {"Sort", "price"},
+                {"Keywords", keywords}
+            };
 
-			return signedHelper.Sign(parameters);
-		}
+            return signedHelper.Sign(parameters);
+        }
 
-		private async Task<XmlDocument> RunQuery(Task<string> url)
-		{
-			var uri = new Uri(url.Result);
-			try
-			{
-				var doc = await XmlDocument.LoadFromUriAsync(uri);
-				return doc;
-			}
-			catch (Exception ex)
-			{
-				Debug.WriteLine(ex.Message);
-			}
+        private async Task<XmlDocument> RunQuery(Task<string> url)
+        {
+            var uri = new Uri(url.Result);
+            try
+            {
+                var doc = await XmlDocument.LoadFromUriAsync(uri);
+                return doc;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
 
-			return null;
-		}
+            return null;
+        }
 
-		private async Task<IList<string>>  ParseResults(Task<XmlDocument> results)
-	    {
-		    var returnValues = new List<string>();
+        private async Task<IList<TitleItem>> ParseResults(Task<XmlDocument> results)
+        {
+            var returnValues = new List<TitleItem>();
 
-		    var doc = results.Result;
+            var doc = results.Result;
 
-		    var errors = doc.GetElementsByTagName("Message");
-			if(errors != null && errors.Count > 0)
-				Debug.WriteLine($"ERRORS: {errors}");
+            var errors = doc.GetElementsByTagName("Message");
+            if (errors != null && errors.Count > 0)
+                Debug.WriteLine($"ERRORS: {errors}");
 
-		    var titles = doc.GetElementsByTagName("Title");
-		    foreach (var title in titles)
-		    {
-			    returnValues.Add(title.InnerText);
-		    }
+            var titles = doc.GetElementsByTagName("Title");
+            foreach (var title in titles)
+            {
+                var value = new TitleItem
+                {
+                    Name = title.InnerText,
+                    ImageUrl = "image"
+                };
 
-		    return returnValues;
-	    }
+                returnValues.Add(value);
+            }
 
-		private async Task ShowValues(Task<IList<string>> values)
-		{
-			foreach (var value in values.Result)
-			{
-				Values.Add(value);
-			}
-		}
-	    
+            return returnValues;
+        }
+
+        private async Task ShowValues(Task<IList<TitleItem>> values)
+        {
+            Values.Clear();
+
+            foreach (var value in values.Result)
+            {
+                Values.Add(value);
+            }
+        }
+
     }
 }
